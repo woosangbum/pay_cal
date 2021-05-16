fcaa1bb5a9dc36ea5b8c10b6ac0486f694bf2ae2
woosangbum <67853792+woosangbum@users.noreply.github.com>
2021-05-16 19:07:36 +0900

    login process example
    practice login pratice

---
+++ pay_cal/index.php
@@ -0,0 +1,14 @@
+<?php
+  include "lib.php";
+
+  session_start();
+
+  $isLogin = $_SESSION["isLogin"];
+  if(!$isLogin){
+    ?>
+    로그인후 이용해 주세요. <br>
+    <a href = "login.php"> 로그인</a>
+
+  <?php 
+    }
+  ?>
